68630827ff90fe57f71fef1bb4f373c5836eedd7
susangithaiga <susangithaiga26@gmail.com>
2024-02-03 02:59:14 +0300

    Complete

---
+++ design/Cards.js
@@ -0,0 +1,9 @@
+// TO BE COMPLETED LATER
+
+const cards = {
+    item01: ["01", "Reviving Retro PCs", "What happens when old PCs are given modern upgrades?"],
+    item02: ["02", "Top 10 Laptops of 2022", "Our best picks for various needs and budgets."],
+    item03: ["03", "The Growth of Gaming", "How the pandemic has sparked fresh opportunities."]
+}
+
+// select card container grid
